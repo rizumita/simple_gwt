98e92e7c41258e8833c458c57053a2844df11146
Ryoichi Izumita <r.izumita@caph.jp>
2022-04-28 07:24:53 +0900

    Create examples

--- example/example.dart
+++ example/example.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_gwt/simple_gwt.dart';
 
@@ -36,4 +37,19 @@ void main() {
     expect(a, 'delayed_a');
     expect(b, 'delayed_b');
   }));
+
+  testWidgets(
+    "WidgetTest",
+    gwt_((tester) async {
+      var result = '';
+      given(
+          'Show TextButton',
+          () => tester.pumpWidget(MaterialApp(
+              home: TextButton(
+                  onPressed: () => result = 'test',
+                  child: const Text('test')))));
+      when('Tap the button', () => tester.tap(find.byType(TextButton)));
+      then('result is \'test\'', () => expect(result, 'test'));
+    }),
+  );
 }
